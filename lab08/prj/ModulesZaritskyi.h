312adf1e5807d39a7b4d1da152bcd17efd1e7791
Victor Zaritsky <viktorzarickiy@gmail.com>
2025-04-09 09:26:21 +0300

    Implemented TestDriver for tasks 10.1-10.3

--- lab08/prj/ModulesZaritskyi.h
+++ lab08/prj/ModulesZaritskyi.h
@@ -11,5 +11,8 @@ double s_calculation(double x, double y, double z);
 double calculateElectricityBill(double kWh);
 Temperature calculateAverageTemperature(const double temps[12]);
 int countBits(int N);
+void processTask10_1(const std::string &inputFileName, const std::string &outputFileName);
+void processTask10_2(const std::string &inputFileName);
+void processTask10_3(const std::string &outputFileName, double x, double y, double z, int b);
 
 #endif // MODULESZARITSKYI_H_INCLUDED
